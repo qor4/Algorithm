d94ffaa45fc26dd1aa56a3a648f3622be831c90b
daeun <58421346+qor4@users.noreply.github.com>
2020-09-05 00:02:10 +0900

    Update 2869 달팽이는 올라가고 싶다.py

--- 9 수학 1/2869 달팽이는 올라가고 싶다.py
+++ 9 수학 1/2869 달팽이는 올라가고 싶다.py
@@ -1,11 +1,8 @@
 up, down, height = map(int, input().split())
+day = (height - up) // (up - down)
 if height <= up:
     print(1)
-elif height % (up - down) == 0:
-    if height % (up - down) + down <= up:
-        print((height // (up - down)) - 1)
-    else:
-        print(height // (up - down))
-elif height % (up - down) != 0:
-    if height % (up - down) <= up:
-        print(height // (up - down) + 1)
+elif (height - up) % (up - down) == 0:
+    print(day + 1)
+elif (height - up) % (up - down) != 0:
+    print(day + 2)
