7687b2c25aa6e94988ec18d9a5f9bfdfa1ec9d81
qor4 <qor_4@naver.com>
2021-02-26 02:35:27 +0900

    11. 격자판 회문수

--- Inflearn_Python-Algorithm-Problem-Solving/섹션 3/11. 격자판 회문수.py
+++ Inflearn_Python-Algorithm-Problem-Solving/섹션 3/11. 격자판 회문수.py
@@ -4,3 +4,15 @@ state = 1
 for i in range(7):
     num.append(list(map(int, input().split())))
 
+count = 0
+for i in range(7):
+    for j in range(3):
+        if num[i][j]==num[i][j+4] and num[i][j+1]==num[i][j+3]:
+            count += 1
+
+for i in range(7):
+    for j in range(3):
+        if num[j][i]==num[j+4][i] and num[j+1][i]==num[j+3][i]:
+            count += 1
+
+print(count)
