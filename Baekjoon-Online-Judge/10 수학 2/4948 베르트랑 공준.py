837a72357dc6c09056a7ec7a6b9db4f043cf4d5e
qor4 <qor_4@naver.com>
2021-03-10 23:40:40 +0900

    4948 베르트랑 공준

--- Baekjoon-Online-Judge/10 수학 2/4948 베르트랑 공준.py
+++ Baekjoon-Online-Judge/10 수학 2/4948 베르트랑 공준.py
@@ -3,8 +3,8 @@ while(True):
     n = int(input())
     if n == 0:
         break
+    count = 0
     for i in range(n+1, (2*n)+1):
-        count = 0
         for j in range(2, i):
             if i % j == 0:
                 break
