4e79652c845b77ede5a16634e77447480b3277a7
daeun <58421346+qor4@users.noreply.github.com>
2021-08-04 16:25:34 +0900

    Update 소수 만들기 (Summer, Winter Coding(~2018)).py

--- Programmers/Level 1/소수 만들기 (Summer, Winter Coding(~2018)).py
+++ Programmers/Level 1/소수 만들기 (Summer, Winter Coding(~2018)).py
@@ -3,10 +3,15 @@ def solution(nums):
     for i in range(len(nums) - 2):
         for j in range(i + 1, len(nums) - 1):
             for k in range(j + 1, len(nums)):
-                for l in range(2, max(nums[i],nums[j],nums[k])):
+                for l in range(2, nums[i] + nums[j] + nums[k]):
                     if (nums[i] + nums[j] + nums[k]) % l == 0:
                         break
                 else:
                     answer += 1
                         
     return answer
+
+
+''' 
+다른 사람의 풀이 : 리스트의 모든 조합을 구하는 라이브러리 from itertools import combinations as cb
+'''
