652b4bd5431e94c3cd1cdc3c2c7cb0f5ff1247d7
daeun <58421346+qor4@users.noreply.github.com>
2022-03-06 23:05:02 +0900

    Create 주식가격.py

---
+++ Programmers/stack, queue/주식가격.py
@@ -0,0 +1,12 @@
+def solution(prices):
+    answer = []
+    for i in range(len(prices)):
+        cnt = 0
+        for j in range(i+1, len(prices)):
+            if prices[j] >= prices[i]:
+                cnt += 1
+            else:
+                cnt += 1
+                break
+        answer.append(cnt)
+    return answer
